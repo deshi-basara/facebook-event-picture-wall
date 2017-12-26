616aab562d3eaa5fef89fdab60c8ab2a2d424fe4
deshi-basara <simon@i-was-perfect.net>
2017-12-26 20:15:18 +0100

    update: Server api path changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://fb.i-was-perfect.net/api',
+  facebook: {
+    appId: '568277443517279',
+    xfbml: true,
+    version: 'v2.11',
+    graphUrl: 'https://graph.facebook.com/v2.8/',
+  },
 };
